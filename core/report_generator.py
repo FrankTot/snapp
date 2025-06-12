031154ef53c8333760da4924828ee53e684e8fc4
FrankTot <francesco.totaro@edu-its.it>
2025-06-12 12:08:28 +0200

    Update report_generator.py

--- core/report_generator.py
+++ core/report_generator.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF
 from datetime import datetime
+import os
 from .system_snapshot import (
     get_active_services,
     get_logged_users,
@@ -12,8 +13,11 @@ class PDFReport(FPDF):
         super().__init__()
         self.set_auto_page_break(auto=True, margin=15)
 
-        # Usa font base predefinito, senza aggiungere DejaVu
-        self.set_font("Helvetica", "", 12)
+        # Mantieni font DejaVu
+        font_path = "assets/DejaVuSans.ttf"
+        self.add_font("DejaVu", "", font_path, uni=True)
+        self.add_font("DejaVu", "B", font_path, uni=True)
+        self.set_font("DejaVu", "", 12)
 
         self.add_page()
         self._add_header()
@@ -24,7 +28,7 @@ class PDFReport(FPDF):
         return datetime.now().strftime("%d-%m-%Y__%H-%M-%S")
 
     def _add_header(self):
-        self.set_font("Helvetica", "B", 16)
+        self.set_font("DejaVu", "B", 16)
         self.set_text_color(30, 30, 30)
         self.cell(0, 10, "📋 SnapAudit Report", 0, 1, 'C')
         self.ln(10)
@@ -36,11 +40,11 @@ class PDFReport(FPDF):
             else:
                 content = "\n".join(str(item) for item in content)
 
-        self.set_font("Helvetica", "B", 14)
+        self.set_font("DejaVu", "B", 14)
         self.set_text_color(40, 90, 160)
         self.cell(0, 10, f"🛡 {title}", 0, 1)
         self.set_text_color(0, 0, 0)
-        self.set_font("Helvetica", "", 11)
+        self.set_font("DejaVu", "", 11)
         self.multi_cell(0, 8, content)
         self.ln()
 
@@ -57,3 +61,7 @@ class PDFReport(FPDF):
         self.add_section("Porte Aperte", get_open_ports())
         self.add_section("Modifiche Recenti in /etc", get_recent_etc_modifications())
         self.output(self.filename)
+
+def create_report(filename=None, icon=False):
+    report = PDFReport(filename)
+    report.generate_full_report()
