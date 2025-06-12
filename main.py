48da4d294931a73d758349e9c26b1a599d0a7aa2
FrankTot <francesco.totaro@edu-its.it>
2025-06-12 12:05:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from PyQt6.QtWidgets import QApplication
 import sys
+import os
 from gui.main_gui import MainGUI
 
 def main():
     if not os.path.exists("reports"):
         os.makedirs("reports")
 
-    app = QApplication(sys.argv)  # PRIMA crei QApplication
-    window = MainGUI()             # POI crei il widget/finestra
+    app = QApplication(sys.argv)
+    window = MainGUI()
     window.show()
     sys.exit(app.exec())
 
