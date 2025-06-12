2e56cb8fff0c514e2b7193a9842df00a96836d4e
FrankTot <francesco.totaro@edu-its.it>
2025-06-12 12:09:19 +0200

    Update pdf_viewer.py

--- gui/pdf_viewer.py
+++ gui/pdf_viewer.py
@@ -4,19 +4,12 @@ import os
 
 def open_pdf(filepath):
     if not os.path.exists(filepath):
-        print(f"Il file {filepath} non esiste.")
+        print(f"Il file {filepath} non esiste")
         return
-
-    system_platform = platform.system()
-
-    try:
-        if system_platform == "Linux":
-            subprocess.Popen(["xdg-open", filepath])
-        elif system_platform == "Darwin":  # macOS
-            subprocess.Popen(["open", filepath])
-        elif system_platform == "Windows":
-            os.startfile(filepath)
-        else:
-            print(f"Sistema non supportato per l'apertura automatica: {system_platform}")
-    except Exception as e:
-        print(f"Errore nell'apertura del PDF: {str(e)}")
+    system = platform.system()
+    if system == "Windows":
+        os.startfile(filepath)
+    elif system == "Darwin":
+        subprocess.call(["open", filepath])
+    else:
+        subprocess.call(["xdg-open", filepath])
